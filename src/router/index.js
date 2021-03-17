9006b876c9c7b588caa5e611d08969c4953ec64c
AlexTr999 <tronenko.aleksandr@gmail.com>
2021-03-17 20:13:57 +0200

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,69 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import store from '../store'
 
-Vue.use(VueRouter)
+import Login from '../components/Login'
+import Dashboard from '../components/Dashboard'
+import Movies from '../components/Movies'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+import guest from './middleware/guest'
+import auth from './middleware/auth'
+import isSubscribed from './middleware/issubscribed'
+import middlewarePipeline from './middlewarePipeline'
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+Vue.use(Router)
+const router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/login',
+            name: 'login',
+            component: Login,
+            meta: {
+              middleware: [
+                  guest
+              ]
+          }
+        },
+        {
+            path: '/dashboard',
+            name: 'dashboard',
+            component: Dashboard,
+            meta: {
+              middleware: [
+                auth
+              ]
+            },
+            children: [{
+                path: '/dashboard/movies',
+                name: 'dashboard.movies',
+                component: Movies,
+                meta: {
+                  middleware: [
+                    auth,
+                    isSubscribed
+                  ]
+                }
+            }
+        ]
+        }
+    ]
 })
-
+router.beforeEach((to, from, next) => {
+  if (!to.meta.middleware) {
+      return next()
+  }
+  const middleware = to.meta.middleware
+  const context = {
+      to,
+      from,
+      next,
+      store
+  }
+  return middleware[0]({
+      ...context,
+      next: middlewarePipeline(context, middleware, 1)
+  })
+})
 export default router
